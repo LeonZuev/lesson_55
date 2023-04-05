a22a42ac6c29c0c948c17bf80ec6e0d365ec98d9
Leon Zuev <loonatune@gmail.com>
2023-04-05 23:38:31 +0300

    Add indexOfNumber

--- src/hw56/IndexSearch.java
+++ src/hw56/IndexSearch.java
@@ -9,7 +9,24 @@ import java.util.ArrayList;
  */
 public class IndexSearch {
   public static int indexOfNumber(ArrayList<Integer> numbers, int target) {
-
+    int left = 0;
+    int right = numbers.size();
+    while (right - left >  1) {
+      int mid = (left + right)/2;
+      if (numbers.get(mid) == target) {
+        return mid;
+      }
+      if (numbers.get(mid) < target) {
+        left = mid + 1;
+      } else {
+        right = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    if (left < numbers.size() && numbers.get(left) == target) {
+      return left;
+    }
+    return -1;
   }
-
 }
