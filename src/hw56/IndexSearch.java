ca72d4d2eeed1c510cb574788b29418e103249a4
Leon Zuev <loonatune@gmail.com>
2023-04-05 23:42:34 +0300

    Fix bug indexOfNumber

--- src/hw56/IndexSearch.java
+++ src/hw56/IndexSearch.java
@@ -8,18 +8,17 @@ import java.util.ArrayList;
 Используйте в качестве образца задачу 2 из классной работы.
  */
 public class IndexSearch {
+
   public static int indexOfNumber(ArrayList<Integer> numbers, int target) {
     int left = 0;
     int right = numbers.size();
-    while (right - left >  1) {
-      int mid = (left + right)/2;
+    while (right - left > 1) {
+      int mid = (left + right) / 2;
       if (numbers.get(mid) == target) {
         return mid;
       }
       if (numbers.get(mid) < target) {
         left = mid + 1;
-      } else {
-        right = mid + 1;
       } else {
         right = mid;
       }
